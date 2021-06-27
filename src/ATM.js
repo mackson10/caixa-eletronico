5cf6dfc1d7ae9a4518097caefe820d2ea1f9eb94
mackson_mattos <mackson.mattos@gmail.com>
2021-06-27 16:26:35 -0300

    feat: primeiras implementações para ATM /faltando algumas funções

--- src/ATM.js
+++ src/ATM.js
@@ -1,28 +1,42 @@
+module.exports = class ATM {
+  constructor(moneyBillStorages = []) {
+    this.moneyBillStorages = moneyBillStorages;
+    this.activeClientAccount = null;
+  }
 
-module.exports.ATM = class {
-
-    constructor (moneyBillStorages = []) {
-        this.moneyBillStorages = moneyBillStorages;
-        this.activeClientAccount = null;
+  getStorageBalance() {
+    // soma todos valores totais dos moneyBillStorages
+    // retorna o valor em dinheiro que este caixa possui
+    let storageBalance = 0;
+    for (let billStorage of this.moneyBillStorages) {
+      storageBalance += billStorage.getTotalValue();
     }
 
-    setActiveClientAccount(clientAccount) {
-        // troca de conta logada
-    }
+    return storageBalance;
+  }
 
-    withdraw (value){
-        // lança uma excessão se não houver usuário logado
-        // verifica se o caixa eletrônico possui o valor em dinheiro
-        // verifica se o cliente tem o saldo em sua conta
-        // calcula a menor quantidade de notas disponiveis para pagar
-        // subtrai o valor do saldo da conta do cliente
-        // subtrai o dinheiro do inventario do caixa eletronico
-        // entrega as notas ao cliente
-    }
+  setActiveClientAccount(clientAccount) {
+    // troca de conta logada
+    if (clientAccount === undefined)
+      throw new Error("É preciso passar uma conta de cliente");
+    else this.activeClientAccount = clientAccount;
+  }
 
-    getBalance() {
-        // soma todos valores totais dos moneyBillStorages
-        // retorna o valor em dinheiro que este caixa possui
-    }
+  getActiveClientAccount() {
+    // retorna de conta logada
+    return this.activeClientAccount;
+  }
 
-}
+  issueWithdraw(value) {
+    // lança uma excessão se não houver usuário logado
+    // verifica se o caixa eletrônico possui o valor em dinheiro
+    // verifica se o cliente tem o saldo em sua conta
+    //
+    // subtrai o valor do saldo da conta do cliente
+    //
+    // calcula a menor quantidade de notas disponiveis para pagar
+    // subtrai o dinheiro do inventario do caixa eletronico
+    //
+    // entrega as notas ao cliente
+  }
+};
