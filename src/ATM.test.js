3d265b2eff222ce90fe701a82f08394ea83ea18a
mackson_mattos <mackson.mattos@gmail.com>
2021-06-27 20:51:20 -0300

    chore: adicionando novos testes em ATM

--- src/ATM.test.js
+++ src/ATM.test.js
@@ -2,14 +2,6 @@ const ATM = require("./ATM");
 const MoneyBillStorage = require("./MoneyBillStorage");
 const ClientAccount = require("./ClientAccount");
 
-jest.mock("./MoneyBillStorage");
-jest.mock("./ClientAccount");
-
-beforeEach(() => {
-  MoneyBillStorage.mockClear();
-  ClientAccount.mockClear();
-});
-
 describe("ATM", function () {
   describe("método getStorageBalance", function () {
     test("deve retornar a soma do valor total das cédulas armazenadas", () => {
@@ -21,15 +13,9 @@ describe("ATM", function () {
 
       let ATM1 = new ATM([billStorage1, billStorage2, billStorage3]);
 
-      MoneyBillStorage.prototype.getTotalValue.mockReturnValue(150);
-      expect(ATM1.getStorageBalance()).toBe(450);
-
-      MoneyBillStorage.prototype.getTotalValue.mockReturnValue(100);
-      expect(ATM1.getStorageBalance()).toBe(300);
-
-      billStorage1.getTotalValue.mockReturnValue(100);
-      billStorage2.getTotalValue.mockReturnValue(15);
-      billStorage3.getTotalValue.mockReturnValue(7000);
+      billStorage1.getTotalValue = jest.fn().mockReturnValue(100);
+      billStorage2.getTotalValue = jest.fn().mockReturnValue(15);
+      billStorage3.getTotalValue = jest.fn().mockReturnValue(7000);
       expect(ATM1.getStorageBalance()).toBe(7115);
 
       let ATM2 = new ATM([]);
@@ -73,6 +59,196 @@ describe("ATM", function () {
 
       ATM1.activeClientAccount = null;
       expect(ATM1.activeClientAccount).toBe(null);
+    });
+  });
+
+  describe("método validateWithdraw", function () {
+    test("deve lançar uma exceção se não houver usuário ativo no caixa", () => {
+      let ATM1 = new ATM([new MoneyBillStorage(10, 10)]);
+      ATM1.activeClientAccount = null;
+      expect(() => ATM1.validateWithdraw(10)).toThrow();
+    });
+
+    test("deve lançar uma exceção se o caixa não tiver fundos suficientes para a retirada", () => {
+      let ATM1 = new ATM([
+        new MoneyBillStorage(20, 5),
+        new MoneyBillStorage(100, 10),
+      ]);
+      let clientAccount1 = new ClientAccount("Ívo Fernandes", 6300);
+
+      ATM1.setActiveClientAccount(clientAccount1);
+
+      expect(() => ATM1.validateWithdraw(4000)).toThrow();
+    });
+
+    test("deve lançar uma exceção se a conta do usuário não possui saldo suficiente para a retirada", () => {
+      let ATM1 = new ATM([
+        new MoneyBillStorage(20, 5),
+        new MoneyBillStorage(100, 10),
+      ]);
+
+      let clientAccount1 = new ClientAccount("Ívo Fernandes", 300);
+      ATM1.setActiveClientAccount(clientAccount1);
+
+      expect(() => ATM1.validateWithdraw(4000)).toThrow();
+    });
+  });
+
+  describe("método computeStorageBillsToPay", function () {
+    test("deve retornar um array de arrays em que a primeira posição é o valor da cédula e a segunda é a quantidade de cédulas a serem pagas", () => {
+      let ATM1 = new ATM([
+        new MoneyBillStorage(50, 3),
+        new MoneyBillStorage(100, 4),
+        new MoneyBillStorage(20, 5),
+        new MoneyBillStorage(10, 3),
+      ]);
+
+      let clientAccount1 = new ClientAccount("Ívo Fernandes", 500);
+      ATM1.setActiveClientAccount(clientAccount1);
+
+      expect(ATM1.computeStorageBillsToPay(460)).toStrictEqual([
+        [100, 4],
+        [50, 1],
+        [10, 1],
+      ]);
+    });
+
+    test("deve lançar uma exceção se o caixa não tem como pagar o valor exato com as cédulas mantidas", () => {
+      let ATM1 = new ATM([
+        new MoneyBillStorage(50, 10),
+        new MoneyBillStorage(100, 5),
+      ]);
+
+      let clientAccount1 = new ClientAccount("Ívo Fernandes", 15);
+      ATM1.setActiveClientAccount(clientAccount1);
+
+      expect(() => ATM1.computeStorageBillsToPay(110)).toThrow();
+    });
+  });
+
+  describe("método releaseMoneyBills", function () {
+    test("deve subtrair a quantidade de cédulas de cada storage", () => {
+      const moneyBillStorage100 = new MoneyBillStorage(100, 4);
+      const moneyBillStorage50 = new MoneyBillStorage(50, 3);
+      const moneyBillStorage20 = new MoneyBillStorage(20, 5);
+      const moneyBillStorage10 = new MoneyBillStorage(10, 3);
+
+      moneyBillStorage100.subtractBills = jest.fn(
+        moneyBillStorage100.subtractBills
+      );
+
+      moneyBillStorage50.subtractBills = jest.fn(
+        moneyBillStorage50.subtractBills
+      );
+
+      moneyBillStorage20.subtractBills = jest.fn(
+        moneyBillStorage20.subtractBills
+      );
+
+      moneyBillStorage10.subtractBills = jest.fn(
+        moneyBillStorage10.subtractBills
+      );
+
+      let ATM1 = new ATM([
+        moneyBillStorage50,
+        moneyBillStorage100,
+        moneyBillStorage20,
+        moneyBillStorage10,
+      ]);
+
+      ATM1.releaseMoneyBills([
+        [100, 3],
+        [20, 4],
+        [10, 2],
+      ]);
+
+      expect(moneyBillStorage100.subtractBills).toHaveBeenCalledWith(
+        3
+      );
+      expect(moneyBillStorage50.subtractBills).toHaveBeenCalledTimes(
+        0
+      );
+      expect(moneyBillStorage20.subtractBills).toHaveBeenCalledWith(
+        4
+      );
+      expect(moneyBillStorage10.subtractBills).toHaveBeenCalledWith(
+        2
+      );
+    });
+  });
+
+  describe("método issueWithdraw", function () {
+    test("deve validar a retirada", () => {
+      let ATM1 = new ATM([new MoneyBillStorage(100, 100)]);
+
+      let clientAccount1 = new ClientAccount("Ívo Fernandes", 1000);
+      ATM1.setActiveClientAccount(clientAccount1);
+
+      ATM1.validateWithdraw = jest.fn();
+
+      const value = 1000;
+
+      ATM1.issueWithdraw(value);
+
+      expect(ATM1.validateWithdraw).toHaveBeenCalledWith(value);
+    });
+
+    test("deve computar as cédulas para o pagamento", () => {
+      let ATM1 = new ATM([new MoneyBillStorage(100, 150)]);
+
+      let clientAccount1 = new ClientAccount("Ívo Fernandes", 4000);
+      ATM1.setActiveClientAccount(clientAccount1);
+
+      const value = 2000;
+
+      ATM1.computeStorageBillsToPay = jest.fn(() => []);
+
+      ATM1.issueWithdraw(value);
+
+      expect(ATM1.computeStorageBillsToPay).toHaveBeenCalledWith(
+        value
+      );
+    });
+
+    test("deve fazer a retirada da conta do cliente ativo", () => {
+      let ATM1 = new ATM([new MoneyBillStorage(100, 110)]);
+
+      let clientAccount1 = new ClientAccount("Ívo Fernandes", 5000);
+      ATM1.setActiveClientAccount(clientAccount1);
+
+      clientAccount1.withdraw = jest.fn();
+
+      ATM1.getActiveClientAccount = jest.fn(() => clientAccount1);
+
+      const value = 4000;
+
+      ATM1.issueWithdraw(value);
+
+      expect(ATM1.getActiveClientAccount).toHaveBeenCalled();
+      expect(clientAccount1.withdraw).toHaveBeenCalledWith(value);
+    });
+
+    test("deve liberar as cédulas como definido pelo método computeStorageBillsToPay", () => {
+      let ATM1 = new ATM([new MoneyBillStorage(100, 10)]);
+
+      let clientAccount1 = new ClientAccount("Ívo Fernandes", 5000);
+      ATM1.setActiveClientAccount(clientAccount1);
+
+      const mockedMoneyPackage = Symbol("MockedmoneyPackage");
+
+      ATM1.computeStorageBillsToPay = jest.fn(
+        () => mockedMoneyPackage
+      );
+
+      ATM1.releaseMoneyBills = jest.fn(() => {});
+
+      const value = 1000;
+
+      ATM1.issueWithdraw(value);
+
+      expect(ATM1.releaseMoneyBills).toHaveBeenCalledWith(
+        mockedMoneyPackage
+      );
     });
   });
 });
