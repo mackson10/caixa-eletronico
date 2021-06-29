acf11606c5814bb19a2a52c17bbcdf797d59ed95
mackson_mattos <mackson.mattos@gmail.com>
2021-06-29 00:20:49 -0300

    feat: classe Main organizada e finalizada

--- src/Main.js
+++ src/Main.js
@@ -3,49 +3,42 @@ const ClientAccount = require("./ClientAccount");
 const rl = require("./lib/readline");
 
 module.exports = class Main {
-  constructor(billStorages) {
-    this.myATM = new ATM(billStorages);
-    this.running = true;
+  constructor({
+    defaultBillStorages,
+    defaultAccountClientName,
+    defaultAccountClientBalance,
+  }) {
+    this.myATM = new ATM(defaultBillStorages);
+    this.defaultAccountClientName = defaultAccountClientName;
+    this.defaultAccountClientBalance = defaultAccountClientBalance;
   }
 
   async init() {
-    const clientName = await rl.questionPromise(
-      `######################################\n\n` +
-      `Bem vindo ao Caixa eletrônico do Banco Sósaque™ \n\n` +
-      `######################################\n\n` +
-      `Seu nome(default: João): `
-    );
-    const clientBalance = await rl.questionPromise(
-      `Seu saldo(default: Infinito): `
-    );
-
-    this.clientAccount = new ClientAccount(
-      clientName || "João da Silva",
-      +clientBalance || Infinity
-    );
-
-    this.myATM.setActiveClientAccount(this.clientAccount);
-
+    this.running = true;
+    await this.greetClient();
     while (this.running) await this.loop();
-
-    console.log("\nAté mais " + this.clientAccount.clientName + "! \n\n Banco Sósaque™, não aceitamos depósitos!")
-
+    this.sayGoodbye();
+    this.myATM.setActiveClientAccount(null);
     process.exit(0);
   }
 
   async loop() {
     try {
-      const clientBalance =
-        this.clientAccount.getBalance() !== Infinity
-          ? `R$${this.clientAccount.getBalance().toFixed(2)}`
+      const activeClient = this.myATM.getActiveClientAccount();
+      const clientName = activeClient.clientName;
+      const clientBalance = activeClient.getBalance();
+
+      const formattedClientBalance =
+        clientBalance !== Infinity
+          ? `R$${clientBalance.toFixed(2)}`
           : "Infinito";
 
       const chosenOperation = await rl.questionPromise(
-        `\n Cliente: ${this.clientAccount.clientName} -- Saldo: ${clientBalance} \n\n` +
-        `Operações disponíveis: \n\n` +
-        `1 - Saque \n` +
-        `2 - Mostrar níves do estoque de cédulas \n` +
-        `3 - Sair \n\n`
+        `\n Cliente: ${clientName} -- Saldo: ${formattedClientBalance} \n\n` +
+          `Operações disponíveis: \n\n` +
+          `1 - Saque \n` +
+          `2 - Mostrar níves do estoque de cédulas \n` +
+          `3 - Sair \n\n`
       );
 
       if (chosenOperation == 1) {
@@ -54,24 +47,44 @@ module.exports = class Main {
         );
 
         this.myATM.issueWithdraw(+withdrawValue);
-
       } else if (chosenOperation == 2) {
-
         this.myATM.showBillStorageLevels();
-
       } else if (chosenOperation == 3) {
-
         this.running = false;
-
       } else {
-
         console.log(
           "\nOperação não implementada, tente novamente. \n"
         );
-
       }
     } catch (error) {
       console.error("( !!! ) Houve um erro: " + error.message);
     }
+  }
+
+  async greetClient() {
+    const clientName = await rl.questionPromise(
+      `######################################\n\n` +
+        `Bem vindo ao Caixa eletrônico do Banco Sósaque™ \n\n` +
+        `######################################\n\n` +
+        `Seu nome(default: ${this.defaultAccountClientName}): `
+    );
+    const clientBalance = await rl.questionPromise(
+      `Seu saldo(default: ${this.defaultAccountClientBalance}): `
+    );
+
+    this.myATM.setActiveClientAccount(
+      new ClientAccount(
+        clientName || this.defaultAccountClientName,
+        +clientBalance || this.defaultAccountClientBalance
+      )
+    );
+  }
+
+  sayGoodbye() {
+    console.log(
+      "\nAté mais " +
+        this.myATM.getActiveClientAccount().clientName +
+        "! \n\n Banco Sósaque™, não aceitamos depósitos!"
+    );
   }
 };
